6e486f46c5db6fe4dcbee29a1ea48b68fbb3c339
Igor_Sidorov <igor.sidorov.190795@gmail.com>
2021-06-02 15:24:11 +0300

    Added get one Employee

--- src/main/java/com/sidorov/igor/spring/rest/Rabbit/RabbitMqListener.java
+++ src/main/java/com/sidorov/igor/spring/rest/Rabbit/RabbitMqListener.java
@@ -1,18 +1,31 @@
 package com.sidorov.igor.spring.rest.Rabbit;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sidorov.igor.spring.rest.Service.EmployeeService;
+import com.sidorov.igor.spring.rest.controller.ControllerEmployee;
+import com.sidorov.igor.spring.rest.entity.Employee;
 import org.apache.log4j.Logger;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @EnableRabbit
 @Component
 public class RabbitMqListener {
     Logger logger = Logger.getLogger(RabbitMqListener.class);
 
+    @Autowired
+    private EmployeeService employeeService;
+
     @RabbitListener(queues = "queue1")
-    public void processQueue1(String message) throws InterruptedException {
-        logger.info("Received from queue 1: " + message);
-        Thread.sleep(20_000);
+    public String processQueue1(String message) throws InterruptedException, JsonProcessingException {
+        logger.info("Get message : " + message);
+        ObjectMapper objectMapper = new ObjectMapper();
+        return message.equals("ShowAllEmployees") ? objectMapper.writeValueAsString(employeeService.getAllEmployees())
+                : objectMapper.writeValueAsString(employeeService.getEmployee(Integer.parseInt(message)));
     }
 }
